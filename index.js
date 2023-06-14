afcffc475ab75377a02052666fdafc99c8870c21
sanimations <smartucci157@gmail.com>
2023-06-14 19:59:11 -0400

    practice git commit in terminal

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express'),
+    morgan = require('morgan');
+
+const app = express();
+
+let myLogger = (req, res, next) => {
+    console.log(req.url);
+    next();
+};
+
+let requestTime = (req, res, next) => {
+    req.requestTime = Date.now();
+    next();
+};
+
+app.use(myLogger);
+app.use(requestTime);
+
+app.get('/', (req, res)=> {
+    let responseText = 'Welcome to my app!';
+    responseText += '<small>Requested at: ' + req.requestTime + '<small>';
+    res.send(responseText);
+});
+
+app.get('/secreturl', (req, res) => {
+    let responseText = 'This is a secret url with super top-secret content.';
+    responseText += '<small>Requested at: ' + req.requestTime + '<small>';
+    res.send(responseText);
+});
+
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});
