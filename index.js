13afd7233ce59d102ac0c600a2ff6d8d2142f40f
sanimations <smartucci157@gmail.com>
2023-06-14 21:26:28 -0400

    finalized index.js with movie list

--- index.js
+++ index.js
@@ -3,31 +3,103 @@ const express = require('express'),
 
 const app = express();
 
-let myLogger = (req, res, next) => {
-    console.log(req.url);
-    next();
-};
+let queerFilms = [
+    {
+        title: 'Tangerine',
+        director: 'Sean Baker'
+    },
+    {
+        title: 'Happy Together',
+        director: 'Wong Kar-Wai'
+    },
+    {
+        title:'Love, Simon',
+        director: 'Greg Berlanti'
+    },
+    {
+        title: 'The Watermelon Woman',
+        director: 'Cherly Dunye'
+    },
+    {
+        title: 'Saving Face',
+        director: 'Alice Wu'
+    },
+    {
+        title: 'But I\'m a Cheerleader',
+        director: 'Jamie Babbit'
+    },
+    {
+        title: 'Moonlight',
+        director: 'Barry Jenkins'
+    },
+    {
+        title: 'Portrait of a Lady on Fire',
+        director: 'Céline Sciamma'
+    },
+    {
+        title: 'Paris is Burning',
+        director: 'Jennie Livingston'
+    },
+    {
+        title: 'Pariah',
+        director: 'Dee Rees'
+    },
+    {
+        title: 'Beginners',
+        director: 'Mike Mills'
+    }
+]
 
-let requestTime = (req, res, next) => {
-    req.requestTime = Date.now();
-    next();
-};
+app.use(morgan('common'));
 
-app.use(myLogger);
-app.use(requestTime);
+app.get('/movies', (req, res) => {
+    res.json(queerFilms);
+})
 
-app.get('/', (req, res)=> {
-    let responseText = 'Welcome to my app!';
-    responseText += '<small>Requested at: ' + req.requestTime + '<small>';
-    res.send(responseText);
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my app');
 });
 
-app.get('/secreturl', (req, res) => {
-    let responseText = 'This is a secret url with super top-secret content.';
-    responseText += '<small>Requested at: ' + req.requestTime + '<small>';
-    res.send(responseText);
+app.get('documentation.html', (req, res) => {
+    res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something is broken!');
 });
 
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
 });
+
+
+
+
+    //These should all be beefore app.listen, leaving these here in case morgan doesn't work
+// let myLogger = (req, res, next) => {
+//     console.log(req.url);
+//     next();
+// };
+
+// let requestTime = (req, res, next) => {
+//     req.requestTime = Date.now();
+//     next();
+// };
+
+// app.use(myLogger);
+// app.use(requestTime);
+
+// app.get('/', (req, res)=> {
+//     let responseText = 'Welcome to my app!';
+//     responseText += '<small>Requested at: ' + req.requestTime + '<small>';
+//     res.send(responseText);
+// });
+
+// app.get('/secreturl', (req, res) => {
+//     let responseText = 'This is a secret url with super top-secret content.';
+//     responseText += '<small>Requested at: ' + req.requestTime + '<small>';
+//     res.send(responseText);
+// });
