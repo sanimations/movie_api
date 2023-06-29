f2ae1f704503bb3366b31b396aa29e9295d00f4b
sanimations <smartucci157@gmail.com>
2023-06-29 04:01:00 -0400

    Added models.js file and updated index.js with models

--- index.js
+++ index.js
@@ -2,145 +2,44 @@ const express = require('express'),
     morgan = require('morgan'),
     fs = require('fs'),
     path = require('path'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    Models = require('./models.js');
 
+const Movies = Models.Movie;
+const Users = Models.User;
+const Genres = Models.Genre;
+const Directors = Models.Director;
+mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
 const app = express();
 
 app.use(bodyParser.json());
-
-let queerFilms = [
-    {
-        title: 'Tangerine',
-        director: 'Sean Baker',
-        genres: ['Drama'],
-        favorites: false
-    },
-    {
-        title: 'Happy Together',
-        director: 'Wong Kar-Wai',
-        genres: ['Drama'],
-        favorites: false       
-    },
-    {
-        title:'Love, Simon',
-        director: 'Greg Berlanti',
-        genres: ['ComingOfAge'],
-        favorites: false
-    },
-    {
-        title: 'Saving Face',
-        director: 'Alice Wu',
-        genres: ['Comedy'],
-        favorites: false
-    },
-    {
-        title: 'But I\'m a Cheerleader',
-        director: 'Jamie Babbit',
-        genres: ['Satire'],
-        favorites: false
-    },
-    {
-        title: 'Moonlight',
-        director: 'Barry Jenkins',
-        genres: ['ComingOfAge'],
-        favorites: false
-    },
-    {
-        title: 'Portrait of a Lady on Fire',
-        director: 'Céline Sciamma',
-        genres: ['Romance'],
-        favorites: false
-    },
-    {
-        title: 'Paris is Burning',
-        director: 'Jennie Livingston',
-        genres: ['Documentary'],
-        favorites: false
-    },
-    {
-        title: 'Pariah',
-        director: 'Dee Rees',
-        genres: ['Drama'],
-        favorites: false
-    },
-    {
-        title: 'Beginners',
-        director: 'Mike Mills',
-        genres: ['Romance'],
-        favorites: false
-    },
-    {
-        title: 'All About My Mother',
-        director: 'Pedro Almadovar',
-        genres: ['Drama'],
-        favorites: false
-    },
-    {
-        title: 'Volver',
-        director: 'Pedro Almadovar',
-        genres: ['Comedy'],
-        favorites: false
-    }
-]
-let users = [
-    {
-        name: 'Sam',
-        username: 'Mas52yaG',
-        email: 'notFakeEmail@yahoo.com',
-        LGBT: 'Queer',
-        id: '1'
-    },
-    {
-        name: 'Utkarsha',
-        username: 'UtBakaksrshiHA',
-        email: 'coolTutors@careerfoundry.com',
-        LGBT: '',
-        id: '2'   
-    },
-    {
-        name: 'Drew',
-        username: 'coDDrew',
-        email: 'mentororing@careerfoundry.com',
-        LGBT: '',
-        id: '3'
-    }
-]
-
-let genreList = [
-    {
-        Comedy: 'professional entertainment consisting of jokes and satirical sketches, intended to make an audience laugh.' 
-    }, 
-    {
-        Romance: 'One or more characters fall in love'
-    },
-    {
-        Satire: 'the use of humor, irony, exaggeration, or ridicule to expose and criticize people\'s stupidity or vices, particularly in the context of contemporary politics and other topical issues.'
-    }, 
-    {
-        ComingOfAge: 'A young person\'s transition from child to adulthood'
-    }, 
-    {
-        Drama: 'an emotional or unexpected series of events or set of circumstances.'
-    }, 
-    {
-        Documentary: 'A documentary film or documentary is a non-fictional motion-picture intended to "document reality, primarily for the purposes of instruction, education or maintaining a historical record'
-    }, 
-    {
-        History: 'Set in a specific time-period that greatly influences the plot'
-    }
-]
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('common'));
 
 //Get list of all movies
 app.get('/movies', (req, res) => {
-    res.json(queerFilms);
-})
+    Movies.find()
+        .then((movies) => {
+            res.status(201).json(movies);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
 
 //Get list of all users
 app.get('/users', (req, res) => {
-    res.json(users);
-})
+    Users.find()
+        .then((users) => {
+            res.status(201).json(users);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
 
 app.use(express.static('public'));
 
@@ -153,15 +52,27 @@ app.get('documentation.html', (req, res) => {
 });
 
 //Gets one movie's data by entering in the title
-app.get('/movies/:title', (req, res) => {
-    res.json(queerFilms.find((movie) =>
-      { return movie.title === req.params.title }));
+app.get('/movies/:Title', (req, res) => {
+    Movies.findOne( {Title: req.params.Title })
+        .then((movie) => {
+            res.json(movie);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 //Gets one director's data by entering in their name
 app.get('/movies/:director', (req, res) => {
-    res.json(queerFilms.find((name) =>
-      { return name.director === req.params.director }));
+    Movies.findOne( {Director: req.params.Director })
+        .then((movie) => {
+            res.json(movie);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 //Gets teh definition of a genre
@@ -170,53 +81,88 @@ app.get('/:genre', (req, res) => {
       { return genre === req.params.genre }));
 });
 
-//Allows a new user to register
+//Allows a new user to register expected in JSON format
 app.post('/users', (req, res) => {
-    let newUser = req.body;
-  
-    if (!newUser.name) {
-      const message = 'Missing name in request body';
-      res.status(400).send(message);
-    } else {
-      users.push(newUser);
-      res.status(201).send(newUser);
-    }
+    Users.findOne({ Username: req.body.Username })
+        .then((user) => {
+            if (user) {
+                return res.status(400).send(req.body.Username + 'already exists');
+            } else {
+                Users
+                    .create({
+                        Username: req.body.Username,
+                        Password: req.body.Password,
+                        Email: req.body.Email,
+                        Birthday: req.body.Birthday
+                    })
+                    .then((user) => { res.status(201).json(user) })
+                    .catch((error) => {
+                        console.error(error);
+                        res.status(500).send('Error: ' + error);
+                    })
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+        });
 });
 
 //Allows users to update their username  
-app.put('/users/:name/:username/', (req, res) => {
-    let user = users.find((user) => { return user.name === req.params.name });
-  
-    if (user) {
-      user.username = req.params.username;
-      res.status(201).send(req.params.username + ' will be the new username');
-    } else {
-      res.status(404).send('User with the username ' + req.params.username + ' was not found.');
-    }
+app.put('/users/:username/', (req, res) => {
+    Users.findOneAndUpdate({ Username: req.params.Username }, {
+        $set:
+        {
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+        }
+    },
+        { new: true }, //This makes sure that the udpated doc is returned in the error section
+        (err, updatedUser) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            } else {
+                res.json(updatedUser);
+            }
+        });
 });
-
 //Allows users to delete their user profile
-app.delete('/users/:id', (req, res) => {
-    let user = users.find((user) => {return user.id === req.params.id});
-
-    if(user){
-        users = users.filter((obj) => { return obj.id !== req.params.id});
-        res.status(201).send('User ' + req.params.id + ' was deleted.');
-    }
+app.delete('/users/:Username', (req, res) => {
+    Users.findOneAndRemove({ Username: req.params.Username })
+        .then((user) => {
+            if (!user) {
+                res.status(400).send(req.params.Username + ' was not found!');
+            } else {
+                res.status(200).send(req.params.Username + ' was deleted.  We\'ll miss you!');
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 //Allows users to add to favorites
-app.put('/movies/:title/:favorites', (req, res) => {
-    let favList = queerFilms.find((favList) => { return favList.title === req.params.title });
-  
-    if (favList) {
-      favList.favorites = req.params.favorites;
-      res.status(201).send( req.params.title + ' was added to your favorites!');
-    } else {
-      res.status(404).send(req.params.title + ' was not found.');
-    }
+app.put('/users/:Username/movies/:MovieID', (req, res) => {
+    app.post('/users/:Username/movies/:MovieID', (req, res) => {
+        Users.findOneAndUpdate({ Username: req.params.Username }, {
+            $push: { FavoriteMovies: req.params.MovieID }
+        },
+            { new: true }, // makes sure updated document is returned
+            (err, updatedUser) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).send('Error: ' + err);
+                } else {
+                    res.json(updatedUser);
+                }
+            });
+    });
 });
-  
+
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
 app.use(morgan('combined',{stream: accessLogStream}));
@@ -229,32 +175,3 @@ app.use((err, req, res, next) => {
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
 });
-
-
-
-
-    //These should all be beefore app.listen, leaving these here in case morgan doesn't work
-// let myLogger = (req, res, next) => {
-//     console.log(req.url);
-//     next();
-// };
-
-// let requestTime = (req, res, next) => {
-//     req.requestTime = Date.now();
-//     next();
-// };
-
-// app.use(myLogger);
-// app.use(requestTime);
-
-// app.get('/', (req, res)=> {
-//     let responseText = 'Welcome to my app!';
-//     responseText += '<small>Requested at: ' + req.requestTime + '<small>';
-//     res.send(responseText);
-// });
-
-// app.get('/secreturl', (req, res) => {
-//     let responseText = 'This is a secret url with super top-secret content.';
-//     responseText += '<small>Requested at: ' + req.requestTime + '<small>';
-//     res.send(responseText);
-// });
