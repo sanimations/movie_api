543cb3a6fbe428ac802a6f04fe15093ccd285457
sanimations <smartucci157@gmail.com>
2023-07-18 16:52:46 -0400

    Added express-validator and require

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ mongoose.connect('mongodb://127.0.0.1/cfDB?directConnection=true', { useNewUrlPa
 const app = express();
 const Movies = Models.Movie;
 const Users = Models.User;
+const { check, validationResult } = require('express-validator');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
